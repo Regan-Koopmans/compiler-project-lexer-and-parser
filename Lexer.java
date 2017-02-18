0ec95ec948fd541603b8611042b5d9469b086056
Regan-Koopmans <regankoopmans@protonmail.com>
2017-02-18 12:41:00 +0200

    Added basic Main file

---
+++ Lexer.java
@@ -0,0 +1,18 @@
+/*
+
+    CLASS       : Lexer
+    DESCRIPTION : Creates tokens based on given input.
+
+*/
+
+class Lexer {
+  public void lexError(String message) {
+    System.out.println("Lexer Error: " + message);
+  }
+
+  public void scan(String input) {
+
+    // TODO: program lexing logic
+
+  }
+}
---
+++ Main.java
@@ -0,0 +1,56 @@
+/*
+
+    CLASS       : Main
+    DESCRIPTION : Manages the reading of files and provides an entry point to
+                  the Lexer class.
+
+*/
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.lang.StringBuilder;
+
+class Main {
+
+  // Helper method to neaten error messages
+
+  public static void fatalError(String message) {
+    System.out.println(message);
+    System.exit(1);
+  }
+
+  public static void main(String[] args) {
+
+    if (args.length == 0) {
+      fatalError("You did not pass an input file.");
+    }
+
+    // Create an instance of the Lexer class.
+
+    Lexer lex = new Lexer();
+
+    // Create String from file.
+
+    StringBuilder sb = new StringBuilder();
+    File inputFile = new File(args[0]);
+
+    try {
+      FileReader fileReader = new FileReader(inputFile);
+      BufferedReader reader = new BufferedReader(fileReader);
+      String line;
+
+      while ((line = reader.readLine()) != null) {
+        sb.append(line + " ");
+      }
+      fileReader.close();
+
+
+
+    } catch (Exception e) { fatalError("There was a problem reading the file"); }
+
+    // Invoke the Lexers' scan method to scan through the string
+
+    lex.scan(sb.toString());
+  }
+}

f0d1574bb7ab93e9b98b60c3ded466f67ced09ba
Regan-Koopmans <regankoopmans@protonmail.com>
2017-02-20 17:30:47 +0200

    Example if regex is allowed

--- Lexer.java
+++ Lexer.java
@@ -5,13 +5,21 @@
 
 */
 
+
+
 import java.util.ArrayList;
+
+// Regarding to regular expressions.
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class Lexer {
 
   // Helper function to neaten error messages.
 
   private ArrayList<Token> tokens;
+  private String currentWord = "";
 
   public void lexError(String message) {
     System.out.println("Lexer Error: " + message);
@@ -23,9 +31,50 @@ class Lexer {
 
   public void scan(String input) {
 
+    // Flag that manages whether we are currently reading a quoted string
+    boolean parsingString = false;
+
     tokens = new ArrayList<Token>();
 
-    // TODO: program lexing logic
+
+    for (char c:input.toCharArray()) {
+
+      if (c != '\n') {
+        currentWord += c;
+      }
+
+      if (c == '"') {
+        if (parsingString) {
+          parsingString = false;
+          addToken(TokenType.String);
+        } else {
+          parsingString = true;
+        }
+      } else {
+
+        if (matchComparisonOperator(currentWord)) {
+          addToken(TokenType.Comparison);
+        }
+
+        if (matchBooleanOperator(currentWord)) {
+          addToken(TokenType.BooleanOp);
+        }
+
+      }
+    }
+  }
+
+  public boolean matchComparisonOperator(String word) {
+    return word.matches("<|>|(eq)");
+  }
+
+  public boolean matchBooleanOperator(String word) {
+    return word.matches("(and)|(or)|(not)");
+  }
+
+  public void addToken(TokenType type) {
+    tokens.add(new Token(type, currentWord));
+    currentWord = "";
 
   }
 
